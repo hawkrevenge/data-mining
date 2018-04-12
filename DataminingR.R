54844c19b0e71caa5ff44384222c87be95acb515
Roel <hawkrevenge@users.noreply.github.com>
2018-04-12 13:19:07 +0200

    Added boxplot

--- DataminingR.R
+++ DataminingR.R
@@ -1,20 +1,28 @@
-
-checkpackages <- function()
-{
-  library('ggplot2')
-  library('forecast')
-  library('tseries')
-}
-
-installPackages <- function(){
+while(!require('ggplot2'))
   install.packages('ggplot2')
+while(!require('forecast'))
   install.packages('forecast')
+while(!require('tseries'))
   install.packages('tseries')
-  
-  checkpackages()
+
+
+if(!exists("mydata")||is.null(mydata)){
+  mydata <- read.csv("dataset_mood_smartphone.csv")
 }
+mydata$day<-strptime(mydata[,3], "%Y-%m-%d")
 
-tryCatch( checkpackages(), error=installPackages())
+mydata$timeOfDay<-strptime(mydata$time, "%Y-%m-%d %H:%M:%OS")
+mydata$timeOfDay<-format(mydata$timeOfDay, format="%H:%M:%S")
+
+mydata$weekday<-weekdays(mydata$day)
+DaysOfTheWeek<-c( "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+DaysOfTheWeekend<-c("Saturday", "Sunday")
 
 
-mydata = read.csv("dataset_mood_smartphone.csv")
+mydata$weekday <- factor(mydata$weekday, levels= DaysOfTheWeek)
+mydata<-mydata[order(mydata$weekday), ]
+
+
+boxplot(value~weekday,data=mydata[mydata$variable=="mood", ], main="Mood", xlab="Day of the Week", ylab="Mood")
+
+#print(DaysOfTheWeek)
