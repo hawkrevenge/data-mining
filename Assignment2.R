c0773cd9e4384716ca3b9d8165092a9718488ca8
Emma <emmaimmink@hotmail.com>
2018-05-04 12:00:27 +0200

    Assignment 2 added

---
+++ Assignment2.R
@@ -0,0 +1,42 @@
+############################################
+## Datamining VU - Expedia 
+############################################
+
+# Libraries
+library(data.table)  
+library(ggplot2)    
+
+############################################
+## Loading dataset for each user
+############################################
+datatraining.folder  <- 
+  if(grepl("Michael", getwd())) {
+    "C:/Users/Michael"
+  } else if(grepl("Roel", getwd())) {
+    "/Users/Roel"
+  } else if(grepl("Emma", getwd())) {
+    "/Users/Emma/Downloads/Data Mining VU data/training_set_VU_DM_2014.csv"
+  }
+
+
+datatest.folder  <- 
+  if(grepl("Michael", getwd())) {
+    "C:/Users/Michael"
+  } else if(grepl("Roel", getwd())) {
+    "/Users/Roel"
+  } else if(grepl("Emma", getwd())) {
+    "/Users/Emma/Downloads/Data Mining VU data/test_set_VU_DM_2014.csv"
+  }
+
+expedia.data <- fread(datatraining.folder, header=TRUE)       # 32 seconde bij Emma
+expedia.test <- fread(datatest.folder, header=TRUE)   # 37 seconde bij Emma
+
+############################################
+## Exploring dataset
+############################################
+
+dim(expedia.data)
+dim(expedia.predict)
+str(expedia.data)
+#summary(expedia.data)
+
