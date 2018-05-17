e73ab60f8560d2db6fc551cd6dda3116c398dc36
Emma <emmaimmink@hotmail.com>
2018-05-17 12:05:09 +0200

    preprocess funciton werkend

--- Assignment2.R
+++ Assignment2.R
@@ -23,6 +23,11 @@ while(!require('magrittr'))
     install.packages('sjlabelled')
   while(!require('tidyr'))
     install.packages('tidyr')
+  while(!require('xgboost'))
+    install.packages('xgboost')
+  while(!require('caret'))
+    install.packages('caret')
+
   
 
 
@@ -65,9 +70,9 @@ datatest.folder  <<-
 expedia.data <<- fread(datatraining.folder, header=TRUE, na.strings=c("","NULL","NA"))       # 31 seconde bij Emma, 6 bij Roel
 expedia.test <<- fread(datatest.folder, header=TRUE, na.strings=c("","NA"))   # 28 seconde bij Emma, 11 bij Roel
 input_data <<- expedia.data[sample(nrow(expedia.data), 1000), ] 
+test_data <<- expedia.data[sample(nrow(expedia.data), 1000),]
+    
 
-    
-    
 }
 }
 
@@ -322,185 +327,101 @@ preproc<-function(preprocSwitch, input_data, subsample = 0.10){
     input_data = input_data[,!grepl("comp7",names(input_data))]
     input_data = input_data[,!grepl("comp8",names(input_data))]
     
-    # making feature price_quality
-    
+    # making feature normal price, normal locationscore, price_quality
     input_data$normal_price <- (input_data$price_usd / mean(input_data$price_usd))
+    #input_data$normal_location <- (input_data$prop_location_score1 / mean(input_data$prop_location_score1)
     input_data$price_quality <- (input_data$normal_price/ input_data$prop_starrating)
     
   }
-  }
+}
 
-preprocess_data = function(input_data, subsample = 0.10){
+# Read the data
 
-  
-  # - - - - - - - - - - - - - - - 
-  # Fix competition flags
-  # - - - - - - - - - - - - - - -
-  message("Working on competition values features...")
-  # Flags for lowest price and only with availability
-  
-  comp_indices <- which(na_features %in% features[29:52])
-  comp_features <- names(na_df[, comp_indices])
-  comp_df <- na_df[, comp_indices]
-  
-  rate_i <- which(comp_features %in% comp_features[seq(1, length(comp_features), 3)])
-  inv_i <- which(comp_features %in% comp_features[seq(2, length(comp_features), 3)])
-  rpd_i <- which(comp_features %in% comp_features[seq(3, length(comp_features), 3)])
-  
-  n <- nrow(comp_df)
-  lowest_price <- logical(n)
-  only_avail <- logical(n)
-  
-  lp_func <- function(x) (min(x, na.rm=TRUE) > -1 && any(!is.na(x)))
-  oa_func <- function(x) (min(x, na.rm=TRUE) == 1 && any(!is.na(x)))
-  lowest_price <- apply(comp_df[, rate_i], 1, lp_func)
-  only_avail <- apply(comp_df[, inv_i], 1, oa_func)
-  
-  add_comp_df <- data.frame(lowest_price, only_avail)
-  
-  # - - - - - - - - - - - - - - - 
-  # Fix review score
-  # - - - - - - - - - - - - - - -
-  message("Working on review score features...")
-  # Imputing NA review score by value corresponding to 10% quantile
-  input_data$prop_review_score_n <- input_data$prop_review_score
-  
-  property_indices <- which(na_features %in% features[9:15])
-  property_features <- names(na_df[, property_indices])
-  
-  low_review_score <- quantile(na_df$prop_review_score, probs = c(0.1), na.rm=TRUE)
-  
-  
-  #imputing in alterative way the prop_score review
-  #summary by hotels
-  summary.hotels <- input_data %>%
-    group_by(prop_country_id,prop_id) %>%
-    summarise(count=n(), bool_counts_by_prop  = sum(booking_bool),relative_bool= sum(booking_bool)/n(),  clicks_counts_by_prop = sum(click_bool), relative_click= sum(click_bool)/n())
-  
-  
-  input_data <- left_join(input_data,summary.hotels,by=c('prop_country_id','prop_id'))
-  
-  input_data <- input_data %>% group_by(prop_review_score) %>% mutate(normalised_trick3=(clicks_counts_by_prop*bool_counts_by_prop)-mean(clicks_counts_by_prop*bool_counts_by_prop)/sd( clicks_counts_by_prop*bool_counts_by_prop))
-  
-  summary_for_prop_score <- input_data %>%
-    group_by(prop_review_score_n)%>%
-    summarise(number=n(),mean_trick=mean(clicks_counts_by_prop*bool_counts_by_prop),
-              sd_trick=sd(clicks_counts_by_prop*bool_counts_by_prop),
-              mean_trick=mean(clicks_counts_by_prop*bool_counts_by_prop),
-              count_book=sum(booking_bool),count_click=sum(click_bool),
-              book_freq =sum(booking_bool)/number ,click_freq=sum(click_bool)/number,mean_byrscore_group=mean(normalised_trick3,na.rm=T))
-  print(summary_for_prop_score)
-  combinations <- (combn(seq(1,5,0.5),2))
-  
-  
-  # handle missing values of prop_review_score
-  dista <- function(x){
-    min_dist = abs(x^2-groups_centers[1,2]^2)
-    winner=1
-    
-    for (i in seq(2,dim(groups_centers)[1])){
-      if (abs(x-groups_centers[i,2]) < min_dist){
-        min_dist = abs(x^2-groups_centers[i,2]^2)
-        winner=i
-      }
-    }
-    return(as.integer(groups_centers[winner,1]))
-  }
-  
-  groups_centers <- summary_for_prop_score[1:10,c("prop_review_score_n","mean_byrscore_group")]
-  to_substitute <-  apply(input_data[is.na(input_data$prop_review_score_n),"normalised_trick3"],1,FUN =dista)
-  
-  input_data[is.na(input_data$prop_review_score_n),"prop_review_score_n"] <- to_substitute
-  
-  
-  # - - - - - - - - - - - - - - - 
-  # Fix competition flags
-  # - - - - - - - - - - - - - - -
-  message("Working on affinity feature")
-  # Temporary method for imputing affinity, imputing with min
-  input_data$srch_query_affinity_score_2 <- input_data$srch_query_affinity_score
-  
-  
-  affinity <- input_data$srch_query_affinity_score
-  input_data$srch_query_affinity_score[
-    is.na(affinity)] <- min(affinity, na.rm=TRUE)
-  
-  #alternative to impute NA affinity value
-  message("Compute new features to impute NA affinity via fitting hyperbole...")
-  
-  input_data <- input_data %>%
-    mutate(responses = case_when(click_bool == 0 & booking_bool == 0 ~ 0,
-                                 click_bool == 1 & booking_bool == 0 ~ 0.5,
-                                 click_bool == 1 & booking_bool == 1 ~ 1)) %>%
-    mutate(group_size = srch_adults_count + srch_children_count,
-           srch_room_count= , 
-           group_size_normalized_by_room = (srch_adults_count+srch_children_count)/srch_room_count)
-  
-  input_data <- input_data %>% 
-    mutate(trick = ((case_when(promotion_flag == '0' ~ 1, promotion_flag == '1' ~ 2)*case_when(srch_saturday_night_bool == '0' ~ 1, srch_saturday_night_bool == '1' ~ 2))*(srch_length_of_stay*srch_booking_window*group_size))) %>%
-    mutate(affinity = relative_bool * relative_click, affinity_log =  ((relative_bool * relative_click) + count)) %>%
-    mutate(trick2 = ((srch_length_of_stay*srch_booking_window*group_size)+count))
-  
-  fun.1 <- function(x){
-    return ((-350*(1/(sqrt(x))) -12) +rnorm(1))
-  }
-  
-  input_data <- input_data %>%
-    mutate( srch_query_affinity_estimate = fun.1(trick2))
-  
-  input_data$srch_query_affinity_score_2[is.na(input_data$srch_query_affinity_score_2)] <- input_data$srch_query_affinity_estimate[is.na(input_data$srch_query_affinity_score_2)]
-  
-  # - - - - - - - - - - - - - - - 
-  # Altering original data, adding new columns
-  # - - - - - - - - - - - - - - -
-  
-  input_data$prop_review_score[is.na(input_data$prop_review_score)] <- low_review_score
-  input_data$visitor_hist_starrating <- visitor_stars_complete
-  input_data$visitor_hist_adr_usd <- visitor_usd_complete
-  input_data <- cbind(input_data, add_visitor_df, add_comp_df)
-  
-  # - - - - - - - - - - - - - - - - - -
-  # Remove gross_bookings_usd
-  # - - - - - - - - - - - - - - - - - -
-  # Feature is not included in the testing set
-  input_data = dplyr::select(input_data, -gross_bookings_usd)
-  
-  
-  # - - - - - - - - - - - - - - - - - -
-  # Fix NA values for competitors
-  # - - - - - - - - - - - - - - - - - -
-  # Setting every NA to 2
-  
-  imputed_comp_df <- comp_df
-  imputed_comp_df[is.na(comp_df)] <- 2
-  input_data[, seq(29,52)] <- imputed_comp_df
-  
-  
-  # - - - - - - - - - - - -
-  # Subsample the dataframe
-  # - - - - - - - - - - - -
-  message("Subsampling stratified the data...")
-  # Get the number of search queries
-  #subsample_size = length(levels(as.factor(input_data$srch_id))) * subsample
-  
-  # Gather X% of training queries
-  #subsample_idx = sample(levels(as.factor(input_data$srch_id)), size = ceiling(subsample_size))
-  
-  # Keep only sampled queries
-  #input_data_subsampled = input_data %>% 
-  #  filter(srch_id %in% subsample_idx)
-  
-  # Return dataframe with imputed values
-  
-  message("Subsampling stratified the data...")
-  
-  input_data.click <- input_data[input_data$click_bool==1 & input_data$booking_bool == 0,]
-  input_data.book <- input_data[input_data$click_bool==1 & input_data$booking_bool == 1,] 
-  input_data.notbook <- input_data[input_data$click_bool==0 & input_data$booking_bool == 0,]
-  N <- dim(input_data.click)[1]
-  stratified <- rbind(sample_n(input_data.click,size =N,replace=T),sample_n(input_data.book,size=N,replace=T),sample_n(input_data.notbook,size=N,replace=T))
-  
-  return(list(input_data = input_data, 
-              stratified = stratified))
-  #return(input_data_subsampled)
-} 
+train.clean <- read.csv('./data/clean_training_data.csv', header = TRUE, stringsAsFactors = FALSE)
+test.sample <- read.csv('./data/clean_testing_data.csv', header = TRUE, stringsAsFactors = FALSE)
+
+train.clean <- input_data
+test.sample <- test_data
+# Generate random samples for a training and a testing sets
+set.seed(0)
+individual_users <- unique(train.clean$srch_id)
+
+# Select X% of the total population at random
+X = 0.1
+sample_users_pool <- sample(individual_users, (length(individual_users) * X))
+
+sample.train <- unique(sort(sample_users_pool[1:length(sample_users_pool)]))
+
+train.sample <- train.clean[train.clean$srch_id %in% sample.train,]
+
+rm(train.clean, individual_users, sample_users_pool, sample.train)
+
+## Set up the initial model for click_bool
+# Get the initial variables from the text file
+variable.names <- as.vector(names(input_data))
+
+## Model each prediction------------------------
+## Model the click boolean
+xgboost.click_bool <- xgboost(data = sapply(train.sample[variable.names], as.numeric), 
+                              label = train.sample$click_bool,
+                              params = list(objective = "binary:logistic",
+                                            eta = 0.01,
+                                            max.depth = 40,
+                                            nthread = 6),
+                              nrounds = 3,
+                              verbose = 0)
+# Predict click_bool
+xgboost.click_bool.pred <- predict(object = xgboost.click_bool, 
+                                   newdata = sapply(test.sample[variable.names], as.numeric))
+# Add to dataset
+test.sample$click_bool.pred <- ifelse(xgboost.click_bool.pred > 0.489, 1, 0)
+
+# Remove
+rm(xgboost.click_bool, xgboost.click_bool.pred)
+
+
+## Model the booking boolean
+xgboost.booking_bool <- xgboost(data = sapply(train.sample[c(variable.names,'click_bool')], as.numeric), 
+                                label = train.sample$booking_bool,
+                                params = list(objective = "binary:logistic",
+                                              eta = 0.01,
+                                              max.depth = 40,
+                                              nthread = 6),
+                                nrounds = 3,
+                                verbose = 0)
+# predict booking_bool
+xgboost.booking_bool.pred <- predict(object = xgboost.booking_bool, 
+                                     newdata = sapply(test.sample[c(variable.names,'click_bool')], as.numeric))
+# Add to dataset
+test.sample$booking_bool.pred <- ifelse(xgboost.booking_bool.pred > 0.50, 1, 0)
+
+# Remove
+rm(xgboost.booking_bool, xgboost.booking_bool.pred)
+
+## Model the ranking
+xgboost.position <- xgboost(data = sapply(train.sample[c(variable.names,
+                                                         'click_bool', 
+                                                         'booking_bool')], 
+                                          as.numeric), 
+                            label = train.sample$position,
+                            params = list(objective = "rank:pairwise",
+                                          eta = 0.01,
+                                          max.depth = 40,
+                                          nthread = 6,
+                                          eval_metric = 'ndcg'),
+                            nrounds = 3,
+                            verbose = 0)
+# Predict the position in the ranking
+xgboost.position.pred <- predict(object = xgboost.position, 
+                                 newdata = sapply(test.sample[c(variable.names,
+                                                                'click_bool', 
+                                                                'booking_bool')], 
+                                                  as.numeric))
+test.sample$position.pred <- xgboost.position.pred
+test.sample <- test.sample[order(test.sample$srch_id, test.sample$position.pred),]
+test.sample$position.order <- ave(test.sample$position.pred, test.sample$srch_id, FUN = seq_along)
+test.sample <- test.sample[order(test.sample$srch_id, test.sample$position.order),]
+
+# Make the final output file
+final_output <- cbind(test.sample$srch_id, test.sample$prop_id)
+write.csv(final_output, 'FINAL_Output (080).csv')
